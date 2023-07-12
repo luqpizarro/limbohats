e339960afb9e4674e38c5ad38d15759a4fd19dca
luqPizarro <luq.pizarro@gmail.com>
2023-07-13 01:16:49 +0200

    Inicio de la tercera entrega

--- js/app.js
+++ js/app.js
@@ -1,17 +1,3 @@
-let pregunta = prompt('Que estilo de gorra queres? SNAP, TRUCKER o BASEBALL?').toLowerCase()
-console.log(pregunta)
-let contador = 0
-gorras.forEach( (gorra) => {
-    if(pregunta === gorra.tipo) {
-        contador++
-        console.log(contador)
-    }
-})
-let alerta = alert(`Hay ${contador} modelos distintos`)
-console.log(`Hay ${contador} modelos distintos`)
-
-
-
 
 //VARIABLES Y SELECCIONADORES
 let modelo = document.getElementById('modelo');
@@ -19,6 +5,10 @@ let tipo = document.getElementById('tipo');
 let minimo = document.getElementById('minimo');
 let maximo = document.getElementById('maximo');
 let color = document.getElementById('color');
+let botonInicio = document.getElementById('inicioSesion');
+let botonCrearCuenta = document.getElementById('crearCuenta')
+
+
 
 let datosBusqueda = {
     modelo: '',
@@ -28,15 +18,49 @@ let datosBusqueda = {
     color: '',
 };
 
-console.log(datosBusqueda);
+let articulosCarrito = [];
+let usuariosDB = [];
 
+
+
+//CLASSES
+class UI {
+    imprimirMensaje(mensaje, tipo, lugar) {
+        const divMensaje = document.createElement('div');
+        divMensaje.textContent = mensaje;
+
+        tipo === 'error' ? divMensaje.classList.add('alert', 'alert-danger', 'border', 'border-danger', 'text-center', 'border-3', 'text-danger', 'mt-3') : divMensaje.classList.add('alert', 'alert-success', 'border', 'border-success', 'text-center', 'border-3', 'text-success', 'mt-3');
+
+        lugar.appendChild(divMensaje)
+
+        setTimeout(() => {
+            divMensaje.remove();;
+        }, 2000);
+    }
+
+    limpiarHTML() {
+        // Leer el elemento Resultado
+        const contenedor = document.querySelector('#contenedorGorras');
+    
+        // limpiar los resultados anteriores
+        while(contenedor.firstChild) {
+            contenedor.removeChild(contenedor.firstChild);
+        }
+    }
+}
+const ui = new UI;
 
 //EVENT LISTENERS
-
+//EVENTO DE CARGA DE PAGINA
 document.addEventListener('DOMContentLoaded', () => {
+    articulosCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
+    usuariosDB = JSON.parse(localStorage.getItem('usuarios')) || [];
+
     mostrarGorras(gorras);
 })
 
+
+// EVENTOS DE FILTRO DE GORRAS
 modelo.addEventListener('input', e => {
     datosBusqueda.modelo = e.target.value;
     filtarGorra();
@@ -63,25 +87,94 @@ color.addEventListener('input', e => {
     console.log(datosBusqueda);
 })
 
+//EVENTOS DE USUARIO
+botonInicio.addEventListener('click', verificarUsuario);
+botonCrearCuenta.addEventListener('click', almacenarUsuario);
 
+//EVENTOS DE CARRITO
 
 //FUNCIONES
 
-function limpiarHTML() {
-    // Leer el elemento Resultado
-    const contenedor = document.querySelector('#contenedorGorras');
+//Esta parte agrega y valida que los datos de los nuevos usuarios sean correctos
+function almacenarUsuario(e) {
+    e.preventDefault()
 
-    // limpiar los resultados anteriores
-    while(contenedor.firstChild) {
-        contenedor.removeChild(contenedor.firstChild);
+    let usuarioNuevo = document.getElementById('usuarioNuevo').value;
+    let contraseniaNuevo = document.getElementById('contraseniaNuevo').value;
+    let emailNuevo = document.getElementById('emailNuevo').value;
+
+    function validarEmail(email) {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(email);
+    }
+    const emailValido = validarEmail(emailNuevo)
+
+    if (usuarioNuevo === '' || usuarioNuevo.length <= 5 || contraseniaNuevo.length < 8 || contraseniaNuevo === '' || !emailValido) {
+        console.log ('datos incorrecto')
+        ui.imprimirMensaje('Datos Incorrectos', 'error', mensaje)
+    } else {
+        agregarUsuario()
+
+}
+
+function agregarUsuario() {
+    // Verificar si el localStorage ha sido eliminado
+    if (!localStorage.getItem('usuarios')) {
+        usuariosDB = [];
+    }
+
+    const nuevoUsuario = {
+        usuario: usuarioNuevo,
+        contrasenia: contraseniaNuevo,
+        email: emailNuevo
+    };
+
+    usuariosDB.push(nuevoUsuario);
+    localStorage.setItem('usuarios', JSON.stringify(usuariosDB))
+
+    let formulario = document.getElementById('form')
+    formulario.reset ()
+    ui.imprimirMensaje('Usuario Agregado Correctamente', 'exito', mensaje)
+
+
+    setTimeout(() => {
+        location.reload()
+    }, 2500);
     }
 }
 
 
+function verificarUsuario(e) {
+    e.preventDefault();
+
+    let usuario = document.getElementById('usuario').value;
+    let contrasenia = document.getElementById('contrasenia').value;
+    let usuarios = JSON.parse(localStorage.getItem('usuarios'));
+
+    let usuarioEncontrado = usuarios.find( user => user.usuario === usuario && user.contrasenia === contrasenia)
+
+    if(usuarioEncontrado) {
+        console.log(`Hola ${usuario}`)
+        ui.imprimirMensaje('Iniciando sesion...', 'exito', formInicioSesion)
+
+        const userId = document.getElementById('checkUser')
+
+        const divUser = document.createElement('p')
+        divUser.innerHTML = `Hola ${usuario}`
+        userId.appendChild(divUser)
+
+        const formInicio = document.getElementById('formInicioSesion')
+        //formInicio.reset()
+        return;
+    } 
+    
+    usuarios.some( user => user.usuario === usuario && contrasenia != user.contrasenia) ? ui.imprimirMensaje('Contraseña incorrecta', 'error', formInicioSesion) : ui.imprimirMensaje('Usuario No Encontrado, Cree Su Cuenta', 'error', formInicioSesion);
+}
+
 // Esta Funcion muestra las gorras en el html
 function mostrarGorras(gorras) {
     //Reset HTML
-    limpiarHTML();
+    ui.limpiarHTML();   
     //Seleccionar contenedor de gorras
     const contenedor = document.querySelector('#contenedorGorras');
 
@@ -89,43 +182,52 @@ function mostrarGorras(gorras) {
     gorras.forEach(gorra => {
 
         const gorraHTML = document.createElement('div');
-        gorraHTML.classList.add('col-sm-12', 'col-md-5', 'col-lg-3', 'my-4');
+        gorraHTML.classList.add('row', 'justify-content-center', 'align-items-center', 'col-sm-12', 'col-md-5', 'col-lg-3', 'my-4');
         gorraHTML.innerHTML = `
-            <figure class="galeria">
-                <img src="${gorra.img}" class="imgStyle" alt="foto de gorra">
-            </figure>
+            <div class="card" style="width: 18rem;">
+                <img src="${gorra.img}" class="card-img-top" alt="foto de gorra">
+                <div class="card-body">
+                    <h5 class="card-title">${gorra.nombre}</h5>
+                    <p> Precio: $ ${gorra.precio}</p>
+                    <p> Tipo: ${gorra.tipo}</p>
+                    <p> Color: ${gorra.color}</p>
+                    <button type="button" id="${gorra.id}" class="btn btn-primary">Agregar al Carrito</button>
+                </div>
+            </div>
         `;
         contenedor.appendChild(gorraHTML);
+
+        // EVENTO PARA AGREGAR ITEMS AL CARRITO
+        const boton = document.getElementById(`${gorra.id}`)
+        boton.addEventListener('click', () => {
+            agregarCarrito(gorra)
+        })
     });
 }
 
-function error() {
-    limpiarHTML();
-    
-    const contenedor = document.querySelector('#contenedorGorras');
+function agregarCarrito(gorra) {
+    // Verificar si el localStorage ha sido eliminado
+    if (!localStorage.getItem('carrito')) {
+        articulosCarrito = [];
+    }
 
-    const errorHTML = document.createElement('div');
-    errorHTML.innerHTML = `
-    <div class="alert alert-danger text-center" role="alert">
-        Ninguna gorra encontrada
-    </div>
-    `;
-    contenedor.appendChild(errorHTML);
+    articulosCarrito.push(gorra)
+    console.log(articulosCarrito)
 
-    setTimeout(() => {
-        errorHTML.remove();
-        mostrarGorras(gorras);
-    }, 2000);
+    localStorage.setItem('carrito', JSON.stringify(articulosCarrito))
 }
 
+// Esta parte crea la funcion de filtrar la gorra y cada parte del filtro
 function filtarGorra() {
     const resultado = gorras.filter(filtrarModelo).filter(filtrarTipo).filter(filtrarMinimo).filter(filtrarMaximo).filter(filtrarColor);
 
-    if(resultado.length){
-        mostrarGorras(resultado);
+    if(resultado.length) {
+        mostrarGorras(resultado)
     } else {
-        error();
+        ui.limpiarHTML();
+        ui.imprimirMensaje('Ninguna Gorra Encontrada', 'error', contenedorGorras);
     }
+    
 }
 
 function filtrarModelo(gorra) {
--- js/db.js
+++ js/db.js
@@ -1,43 +1,44 @@
 class Gorras {
-    constructor (nombre, modelo, tipo, color, precio, stock, img){
+    constructor (nombre, modelo, tipo, color, precio, stock, img, id){
         this.nombre = nombre;
         this.modelo = modelo;
         this.tipo = tipo;
         this.color = color;
         this.precio = precio;
         this.stock = stock;
-        this.img= img;
+        this.img = img;
+        this.id = id
     }
 };
 
 
-const truckerDiezGris = new Gorras ('trucker diez gris', 'diez', 'trucker', 'gris', 4600, true, './assets/img/GORRAS/$4600/trucker 10 gris.jpg');
-const truckerGatoGris = new Gorras ('trucker gato gris', 'gato', 'trucker', 'gris', 4600, true, './assets/img/GORRAS/$4600/trucker gato gris.jpg');
-const truckerLimboBlancoYNegro = new Gorras ('trucker limbo blanco y negro', 'limbo', 'trucker', 'blanco y negro', 4600, true, './assets/img/GORRAS/$4600/trucker limbo blanco y negro.jpg');
-const truckerLimboVerde = new Gorras ('trucker limbo verde', 'limbo', 'trucker', 'verde', 4600, true, './assets/img/GORRAS/$4600/trucker limbo verde.jpg');
-const truckerTreceNegro = new Gorras ('trucker trece negro', 'trece', 'trucker', 'negro', 5000, true, './assets/img/GORRAS/$5000/trucker 13 negro.jpg');
-const truckerFlorNegro = new Gorras ('trucker flor negro', 'flor', 'trucker', 'negro', 5000, true, './assets/img/GORRAS/$5000/trucker flor negro.jpg');
-const truckerSerpienteBlancoYNegro = new Gorras ('trucker serpiente blanco y negro', 'serpiente', 'trucker', 'blanco y negro', 5000, true, './assets/img/GORRAS/$5000/trucker serpiente blanco y negro.jpg');
-const truckerCalaveraGris = new Gorras ('trucker calavera gris', 'calavera', 'trucker', 'gris', 5000, true, './assets/img/GORRAS/$5000/truker calavera colorida gris.jpg');
-const truckerLMBNegro = new Gorras ('trucker lmb negro', 'limbo', 'trucker', 'negro', 5000, true, './assets/img/GORRAS/$5000/truker lmb negro.jpg');
-const baseballCangrejoNegro = new Gorras ('baseball cangrejo negro', 'cangrejo', 'baseball', 'negro', 5500, true, './assets/img/GORRAS/$5500/baseball cangrejo negro.jpg');
-const baseballFlorGris = new Gorras ('baseball flor gris', 'flor', 'baseball', 'gris', 5500, true, './assets/img/GORRAS/$5500/baseball flor gris.jpg');
-const baseballFlorVioleta = new Gorras ('baseball flor violeta', 'flor', 'baseball', 'violeta', 5500, true, './assets/img/GORRAS/$5500/baseball flor violeta.jpg');
-const baseballIpaVerde = new Gorras ('baseball ipa verde', 'ipa', 'baseball', 'verde', 5500, true, './assets/img/GORRAS/$5500/baseball ipa verde.jpg');
-const snapCalaveraAmarillo = new Gorras ('snap calavera amarillo', 'calavera', 'snap', 'amarillo', 5500, true, './assets/img/GORRAS/$5500/snap calavera amarillo y negro.jpg');
-const snapGatoRojo = new Gorras ('snap gato rojo', 'gato', 'snap', 'rojo', 5500, true, './assets/img/GORRAS/$5500/snap gato rojo.jpg');
-const snapIpaVerde = new Gorras ('snap ipa verde', 'ipa', 'snap', 'verde', 5500, true, './assets/img/GORRAS/$5500/snap ipa verde.jpg');
-const snapLimboHatsNegro = new Gorras ('snap limbo hats negro', 'limbo', 'snap', 'negro', 5500, true, './assets/img/GORRAS/$5500/snap limbo hats negro.jpg');
-const snapLMBArgBlanco = new Gorras ('snap lmb argentina blanco', 'lmb', 'snap', 'blanco', 5500, true, './assets/img/GORRAS/$5500/snap lmb arg blanco.jpg');
-const snapLimboRadioactivoNegro = new Gorras ('snap limbo radioactivo negro', 'limbo', 'snap', 'negro', 5500, true, './assets/img/GORRAS/$5500/snap limbo radioactivo negro.jpg');
-const baseballTreceNegro = new Gorras ('baseball trece negro', 'trece', 'baseball', 'negro', 6500, true, './assets/img/GORRAS/$6500/baseball 13 negro.jpg');
-const baseballCadenaNegro = new Gorras ('baseball cadena negro', 'cadena', 'baseball', 'negro', 6500, true, './assets/img/GORRAS/$6500/baseball cadena negro.jpg');
-const baseballCalaveraAmarillo = new Gorras ('baseball calavera amarillo', 'calavera', 'baseball', 'amarillo', 6500, true, './assets/img/GORRAS/$6500/baseball calavera.jpg');
-const baseballPlanetaGris = new Gorras ('baseball planeta gris', 'planeta', 'baseball', 'gris', 6500, true, './assets/img/GORRAS/$6500/baseball planeta gris.jpg');
-const SnapTreceNegro = new Gorras ('snap trece negro', 'trece', 'snap', 'negro', 6500, true, './assets/img/GORRAS/$6500/snap 13 negro.jpg');
-const SnapCadenaBlancoYNegro = new Gorras ('snap cadena blanco y negro', 'cadena', 'snap', 'blanco y negro', 6500, true, './assets/img/GORRAS/$6500/snap cadena blanca y negra.jpg');
-const SnapCalaveraBlancoYNegro = new Gorras ('snap calavera blanco y negro', 'calavera', 'snap', 'blanco y negro', 6500, true, './assets/img/GORRAS/$6500/snap calavera.jpg');
-const SnapGatoNegro = new Gorras ('snap gato negro', 'gato', 'snap', 'negro', 6500, true, './assets/img/GORRAS/$6500/snap gato grande negro.jpg');
+const truckerDiezGris = new Gorras ('Gorra Maradona', 'diez', 'Trucker', 'Gris', 4600, true, './assets/img/GORRAS/$4600/trucker 10 gris.jpg', 1);
+const truckerGatoGris = new Gorras ('Gorra Gato', 'gato', 'trucker', 'gris', 4600, true, './assets/img/GORRAS/$4600/trucker gato gris.jpg', 2);
+const truckerLimboBlancoYNegro = new Gorras ('Gorra Calavera Limbo', 'limbo', 'trucker', 'blanco y negro', 4600, true, './assets/img/GORRAS/$4600/trucker limbo blanco y negro.jpg', 3);
+const truckerLimboVerde = new Gorras ('Gorra Calavera Limbo', 'limbo', 'trucker', 'verde', 4600, true, './assets/img/GORRAS/$4600/trucker limbo verde.jpg', 4);
+const truckerTreceNegro = new Gorras ('Gorra Numero 13', 'trece', 'trucker', 'negro', 5000, true, './assets/img/GORRAS/$5000/trucker 13 negro.jpg', 5);
+const truckerFlorNegro = new Gorras ('Gorra Flor Tradi', 'flor', 'trucker', 'negro', 5000, true, './assets/img/GORRAS/$5000/trucker flor negro.jpg', 6);
+const truckerSerpienteBlancoYNegro = new Gorras ('Gorra Serpiente', 'serpiente', 'trucker', 'blanco y negro', 5000, true, './assets/img/GORRAS/$5000/trucker serpiente blanco y negro.jpg', 7);
+const truckerCalaveraGris = new Gorras ('Gorra Calavera Limbo', 'calavera', 'trucker', 'gris', 5000, true, './assets/img/GORRAS/$5000/truker calavera colorida gris.jpg', 8);
+const truckerLMBNegro = new Gorras ('Gorra Numero Trece', 'limbo', 'trucker', 'negro', 5000, true, './assets/img/GORRAS/$5000/truker lmb negro.jpg', 9);
+const baseballCangrejoNegro = new Gorras ('Gorra Cangrejo', 'cangrejo', 'baseball', 'negro', 5500, true, './assets/img/GORRAS/$5500/baseball cangrejo negro.jpg', 10);
+const baseballFlorGris = new Gorras ('Gorra Flor Tradi', 'flor', 'baseball', 'gris', 5500, true, './assets/img/GORRAS/$5500/baseball flor gris.jpg', 11);
+const baseballFlorVioleta = new Gorras ('Gorra Flor Tradi', 'flor', 'baseball', 'violeta', 5500, true, './assets/img/GORRAS/$5500/baseball flor violeta.jpg', 12);
+const baseballIpaVerde = new Gorras ('Gorra IPA', 'ipa', 'baseball', 'verde', 5500, true, './assets/img/GORRAS/$5500/baseball ipa verde.jpg', 13);
+const snapCalaveraAmarillo = new Gorras ('Gorra Calavera Skull', 'calavera', 'snap', 'amarillo', 5500, true, './assets/img/GORRAS/$5500/snap calavera amarillo y negro.jpg', 14);
+const snapGatoRojo = new Gorras ('Gorra Gato', 'gato', 'snap', 'rojo', 5500, true, './assets/img/GORRAS/$5500/snap gato rojo.jpg', 15);
+const snapIpaVerde = new Gorras ('Gorra IPA', 'ipa', 'snap', 'verde', 5500, true, './assets/img/GORRAS/$5500/snap ipa verde.jpg', 16);
+const snapLimboHatsNegro = new Gorras ('Gorra Limbo Tattoo', 'limbo', 'snap', 'negro', 5500, true, './assets/img/GORRAS/$5500/snap limbo hats negro.jpg', 17);
+const snapLMBArgBlanco = new Gorras ('Gorra Limbo Argentina', 'limbo', 'snap', 'blanco', 5500, true, './assets/img/GORRAS/$5500/snap lmb arg blanco.jpg', 18);
+const snapLimboRadioactivoNegro = new Gorras ('Gorra Limbo Radioactivo', 'limbo', 'snap', 'negro', 5500, true, './assets/img/GORRAS/$5500/snap limbo radioactivo negro.jpg', 19);
+const baseballTreceNegro = new Gorras ('Gorro Numero 13', 'trece', 'baseball', 'negro', 6500, true, './assets/img/GORRAS/$6500/baseball 13 negro.jpg', 20);
+const baseballCadenaNegro = new Gorras ('Gorra Cadenas', 'cadena', 'baseball', 'negro', 6500, true, './assets/img/GORRAS/$6500/baseball cadena negro.jpg', 21);
+const baseballCalaveraAmarillo = new Gorras ('Gorra Calavera Skull', 'calavera', 'baseball', 'amarillo', 6500, true, './assets/img/GORRAS/$6500/baseball calavera.jpg', 22);
+const baseballPlanetaGris = new Gorras ('Gorra Saturno', 'planeta', 'baseball', 'gris', 6500, true, './assets/img/GORRAS/$6500/baseball planeta gris.jpg', 23);
+const SnapTreceNegro = new Gorras ('Gorra Numero 13', 'trece', 'snap', 'negro', 6500, true, './assets/img/GORRAS/$6500/snap 13 negro.jpg', 24);
+const SnapCadenaBlancoYNegro = new Gorras ('Gorra Cadenas', 'cadena', 'snap', 'blanco y negro', 6500, true, './assets/img/GORRAS/$6500/snap cadena blanca y negra.jpg', 25);
+const SnapCalaveraBlancoYNegro = new Gorras ('Gorra Calavera Skull', 'calavera', 'snap', 'blanco y negro', 6500, true, './assets/img/GORRAS/$6500/snap calavera.jpg', 26);
+const SnapGatoNegro = new Gorras ('Gorra Gato', 'gato', 'snap', 'negro', 6500, true, './assets/img/GORRAS/$6500/snap gato grande negro.jpg', 27);
 
 
 const gorras = [
